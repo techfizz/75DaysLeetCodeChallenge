50361c488be2ce48a5d49e9314de132cc40f5a36
Radhika.S <153347994+techfizz@users.noreply.github.com>
2026-06-17 21:03:13 +0530

    Time: 115 ms (67.39%), Space: 51.6 MB (61.96%) - LeetHub

---
+++ 2280-minimum-lines-to-represent-a-line-chart/2280-minimum-lines-to-represent-a-line-chart.py
@@ -0,0 +1,22 @@
+class Solution:
+    def minimumLines(self, stockPrices: List[List[int]]) -> int:
+        n = len(stockPrices)
+        stockPrices.sort(key = lambda x: (x[0], x[1]))
+        
+        if n == 1:
+            return 0
+        
+        pre_delta_y = stockPrices[0][1] - stockPrices[1][1]
+        pre_delta_x = stockPrices[0][0] - stockPrices[1][0]
+        num = 1
+        
+        for i in range(1, n-1):
+            cur_delta_y = stockPrices[i][1] - stockPrices[i+1][1]
+            cur_delta_x = stockPrices[i][0] - stockPrices[i+1][0]
+            
+            if pre_delta_y * cur_delta_x != pre_delta_x * cur_delta_y:
+                num += 1
+                pre_delta_x = cur_delta_x
+                pre_delta_y = cur_delta_y
+        
+        return num    
